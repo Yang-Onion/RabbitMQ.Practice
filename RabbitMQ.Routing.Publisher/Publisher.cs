b56fc7c92ae8faccb004380f9a8db05e03dfe908
yangchangzhi <690674930@qq.com>
2016-12-26 17:50:58 +0800

    add practice demo

---
+++ RabbitMQ.Routing.Publisher/Publisher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Routing.Publisher
+{
+    public class Publisher
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.2.122",
+                UserName = "Yang",
+                Password = "cms2016...",
+                Port = AmqpTcpEndpoint.UseDefaultPort
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange:"direct_exchange",type:"direct");
+                    var routingKey = args.Length > 0 ? args[0] : "info";
+                    Console.WriteLine($"routingKey:{routingKey}");
+
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange:"direct_exchange",
+                                         routingKey: routingKey,
+                                         basicProperties:null,
+                                         body:body);
+                    Console.WriteLine($" The RabbitMQ.Routing.Publisher Send {message}");
+                }
+            }
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return (args.Length > 1)? string.Join(" ", args.Skip(1).ToArray()): "Hello World!";
+        }
+    }
+}
---
+++ RabbitMQ.Routing.Subscriber/Subscriber.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Routing.Subscriber
+{
+    public class Subscriber
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.2.122",
+                UserName = "Yang",
+                Password = "cms2016...",
+                Port = AmqpTcpEndpoint.UseDefaultPort
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "direct_exchange",
+                                            type: "direct");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    if (args.Length<1)
+                    {
+                        Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",Environment.GetCommandLineArgs()[0]);
+                        Console.WriteLine(" Press [enter] to exit.");
+                        Console.ReadLine();
+                        Environment.ExitCode = 1;
+                        return;
+                    }
+                    foreach (var routingKey in args)
+                    {
+                        Console.WriteLine($" routingKey :{routingKey}");
+                        channel.QueueBind(queue: queueName,
+                                          exchange: "direct_exchange",
+                                          routingKey: routingKey);
+                    }
+
+                    Console.WriteLine(" Subscriber Waiting for logs.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        var routingKey = ea.RoutingKey;
+
+                        Console.WriteLine($" RabbitMQ.Routing.Subscriber routingKeY:{routingKey}, Receive Messge: {message}");
+                    };
+
+                    Console.WriteLine($"queueName:{queueName}");
+                    channel.BasicConsume(queue: queueName,
+                                         noAck: true,
+                                         consumer: consumer);
+
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
